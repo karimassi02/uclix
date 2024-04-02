e02d196c6ad4fa2b87c271367059c87579d497af
assi.karim <karimassi02@gmail.com>
2024-04-02 09:22:49 +0200

    feat: connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 // connection.js
 const mongoose = require("mongoose");
 const Movie = require("../models/movieModel");
-const connection = "mongodb://localhost:27017/UclixDataBase";
+const connection = "mongodb://0.0.0.0:27017/UclixDataBase";
 const connectDb = () => {
- return mongoose.connect(connection);
+  return mongoose.connect(connection);
 };
 module.exports = connectDb;
