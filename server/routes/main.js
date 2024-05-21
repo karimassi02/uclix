cad1c8f7790200ff712d3522583ea3f53411c24e
VincenzoCastrignano <vincenzo.castrignano@esqese.ucly.fr>
2024-05-21 10:24:03 +0200

    mooving img and data + data2 in OLD folder - add categorie page

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,81 +1,87 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Movie = require('../models/movieModel');
+const Movie = require("../models/movieModel");
 
 //Routes
 
 /*
-    * Get/
-    * Home
-*/
-router.get('', async (req,res) => {
-    try {
-        const locals = {
-            title: "Nodejs Blog",
-            description: "Simple Blog created with NodeJS, Express & MongoDb."
-        }
-   
-        const perPage = 5;
-        const page = req.query.page || 1;
+ * Get/
+ * Home
+ */
+router.get("", async (req, res) => {
+  try {
+    const locals = {
+      title: "Nodejs Blog",
+      description: "Simple Blog created with NodeJS, Express & MongoDb.",
+    };
 
-        const data = await Movie.aggregate([{ $sort:{createsAt: -1}}])
-        .skip((perPage * page) - perPage)
-        .limit(perPage)
-        .exec();
+    const perPage = 5;
+    const page = req.query.page || 1;
 
-        const count = await Movie.countDocuments();
-        const nextPage = parseInt(page) + 1 ;
-        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+    const data = await Movie.aggregate([{ $sort: { createsAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
 
-        res.render('index', {
-            locals,
-            data,
-            current: page,
-            nextPage: hasNextPage ? nextPage : null,
-            currentRoute: '/'
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-});
+    const count = await Movie.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
 
-router.get('/home', (req,res) => {
-    res.render('home',{
-        currentRoute: '/home'
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      currentRoute: "/",
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
-router.get('/movie', async (req, res) => {
-    try {
-        const movie = await Movie.find();
-        res.render('movie', { currentRoute: 'movie', movie  });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
+router.get("/home", (req, res) => {
+  res.render("home", {
+    currentRoute: "/home",
+  });
 });
 
-router.get('/movie-details/:id', async (req, res) => {
-    try {
-        const movie = await Movie.findById(req.params.id);
-        res.render('movie-details', {currentRoute: 'movie-details', movie });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal server error');
-    }
+router.get("/categorie", (req, res) => {
+  res.render("categorie", {
+    currentRoute: "/categorie",
+  });
 });
 
-router.get('/top10', (req,res) => {
-    res.render('top10',{
-        currentRoute: '/top10'
-    });
+router.get("/movie", async (req, res) => {
+  try {
+    const movie = await Movie.find();
+    res.render("movie", { currentRoute: "movie", movie });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
-router.get('/people', (req,res) => {
-    res.render('people',{
-        currentRoute: '/people'
-    });
+router.get("/movie-details/:id", async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.render("movie-details", { currentRoute: "movie-details", movie });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+router.get("/top10", (req, res) => {
+  res.render("top10", {
+    currentRoute: "/top10",
+  });
+});
+
+router.get("/people", (req, res) => {
+  res.render("people", {
+    currentRoute: "/people",
+  });
 });
 
 module.exports = router;
